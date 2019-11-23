3dfe6b5e45d964eac9e67219c84c1603e4ac562d
matheusjgspic <matheusjgsilva3530@gmail.com>
2019-11-23 08:28:39 -0300

    9.8.14

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ async def on_command_error(ctx, error):
 		await ctx.send("**[ERRO]** - Comando não encontrado")
 
 @bot.event
-async def on_member_join(ctx, member):
-	server=ctx.message.guild
+async def on_member_join(member):
+	server=message.guild
 	embed = discord.Embed(colour=0x95efcc,
 		description="Bem vindo ao servidor {}! Membro nº**{}**".format(server, len(list(member.guild.members))))
 
